5c9a4def70f7d45725e756abc2dfa8220b89b415
shaohua.liu <fe.liushaohua@gmail.com>
2015-07-28 19:00:15 +0800

    Update
    Update

--- asset/home/index.js
+++ asset/home/index.js
@@ -6,7 +6,6 @@ define(function(require) {
 
     home.beforeRender = function() {
 		//在页面渲染之前执行，获取数据
-
 		console.log('beforeRender');
 	}
 
@@ -14,11 +13,62 @@ define(function(require) {
 		//在页面渲染之后执行，对页面进行操作
 		console.log('initBehavior','uuu');
 		// 使用
-		$(document).trigger('Runner/hashChange')
+		$(document).trigger('Runner/hashChange');
+
+		var Render = {
+			init: function () {
+				this.render_nav();
+			},
+			render_nav: function () {
+				var cHTML = '<li>',
+					data = [{
+					'name': '日期'
+				},{
+					'name': '时段'
+				},{
+					'name': '业务线'
+				},{
+					'name': '日期'
+				},{
+					'name': '日期'
+				},{
+					'name': '时段'
+				},{
+					'name': '日期'
+				},{
+					'name': '业务线'
+				},{
+					'name': '日期'
+				},{
+					'name': '日期'
+				},{
+					'name': '时段'
+				},{
+					'name': '日期'
+				},{
+					'name': '业务线'
+				},{
+					'name': '日期'
+				},{
+					'name': '日期3'
+				}];
+
+				for (var i = 0, len = data.length; i < len; i++) {
+					var bBOOL = ((i+1)% 6 == 0 && i+1 != len);
+					cHTML += '<a href="javascript:;"'+ (bBOOL ? 'class="c-span-last"' : '')+'>' + data[i]['name'] +'</a>'
+					      + ( bBOOL ? '</li><li>'  : '');
+				}
+					cHTML += '</li>';
+				$('.query_wrap ul').html(cHTML);
+			}
+		};
+
+		Render.init();
+
 		require(['echarts/echarts-all','echarts/chart/macarons'],
 			function (ec,theme) {
 				// 基于准备好的dom，初始化echarts图表
-				var myChart = echarts.init(document.getElementById('main2'),theme);
+				var myChart = echarts.init(document.getElementById('main_wrap'),theme);
 
 				var option = [{
 					name:'周一',
@@ -87,7 +137,8 @@ define(function(require) {
 						trigger: 'axis'
 					},
 					legend: {
-						data:['最高气温','最低气温']
+						data:['最高气温','最低气温'],
+						textStyle:{color: '#fff'}
 					},
 					toolbox: {
 						show : true,
@@ -104,14 +155,22 @@ define(function(require) {
 						{
 							type : 'category',
 							boundaryGap : false,
-							data : ['周一','周二','周三','周四','周五','周六','周日']
+							data : ['周一','周二','周三','周四','周五','周六','周日'],
+							axisLabel : {
+								textStyle:{
+									color:"#fff"
+								}
+							}
 						}
 					],
 					yAxis : [
 						{
 							type : 'value',
 							axisLabel : {
-								formatter: '{value} °C'
+								formatter: '{value} °C',
+								textStyle:{
+									color:"#fff"
+								}
 							}
 						}
 					]
